9c5ecbcdaef77f4f6ed462ef76df09934bf7f970
Camila Arenas <mcarenash@Camilas-MacBook-Air.local>
2015-01-26 18:22:08 -0800

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :first_name, :last_name, :address, :city, :state, :zipcode, presence: true
+  # validates :first_name, :last_name, :address, :city, :state, :zipcode, presence: true
 
   has_many :events, dependent: :destroy
   has_many :attendances, dependent: :destroy
