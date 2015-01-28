4f9b6ece721e8e247218e358a1ff5a9b8417d69e
Camila Arenas <mcarenash@Camilas-MacBook-Air.local>
2015-01-27 18:44:03 -0800

    fixed views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,7 @@ class ApplicationController < ActionController::Base
       u.permit(:first_name, :last_name, :address, :city, :state, :zipcode, :email, :password, :password_confirmation)
     end
   end
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || event_path(1)
+  end
 end
