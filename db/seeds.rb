2fd411ce6adaaf5e2ac7262099fd8147ab4f48a0
Albert Sison <asison89@gmail.com>
2015-01-28 18:56:04 -0800

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Event.destroy_all
+
+10.times do |i|
+  Event.create(
+    name: "event#{i}",
+    date: "02/01/2015",
+    address: "225 Bush St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94104",
+    description: "Thank you for coming out to Event #{i}"
+  )
+end
