44ea08e9ee923954ae55bf404748f01a1fb39023
Jonathan Edradan <jmedradan@gmail.com>
2015-01-29 19:10:21 -0800

    Populated the seed.rb for DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,242 @@
 
 Event.destroy_all
 
-10.times do |i|
-  Event.create(
-    name: "event#{i}",
+Event.create(
+    name: "Elders Need Helpers in the San Francisco Community: Volunteer to Help Seniors Today!",
     date: "02/01/2015",
-    address: "225 Bush St",
+    address: "155 Market St",
     city: "San Francisco",
     state: "CA",
-    zipcode: "94104",
-    description: "Thank you for coming out to Event #{i}"
-  )
-end
+    zipcode: "94102",
+    description: "Volunteer to help elders for the activities you enjoy, on a schedule that works best for you. Go to ElderHelpers.org to create a volunteer profile and start today!"
+)
+
+Event.create(
+    name: "Openhouse has Data Entry Opportunities galore!",
+    date: "02/14/2015",
+    address: "1800 Market Street",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94102",
+    description: "Openhouse ( www.openhouse-sf.org) has data entry opportunities and the schedule is flexible! Data entry opportunities includes: * entering information from volunteer applications into volunteer software database * Entering housing workshop evaluations into software designated for that purpose *Entering information and referral into specific database software * Entering information into our special software databases. We are looking for volunteers who are tech savvy, comfortable learning new software databases. Interested? Sure, why not? Call or email Openhouse! (415)296-8995 ext 310 or suzi@openhouse-sf.org."
+)
+
+Event.create(
+    name: "Parent Partner Program Assistant Internship",
+    date: "01/31/2015",
+    address: "3040 16th St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94103",
+    description: "The objective of the Parent Partner Program is to provide monolingual Spanish-speaking and/or immigrant parents with the information, training and confidence to be more actively engaged in supporting their children’s academic success. We accomplish this by offering a range of workshops for parents onsite at target Mission District public schools. We also offer professional development and coaching to a team of parent leaders from these schools that provide logistical and outreach support for our school-based workshops."
+)
+
+Event.create(
+    name: "Volunteer Leadership Position: Explore San Francisco and build community",
+    date: "03/04/2015",
+    address: "466 Geary St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94102",
+    description: "Are you a natural leader overflowing with energy? Do you love San Francsico and Sunday Streets (Don't know Sunday Streets? - check it out here: www.sundaystreetssf.com/video-gallery)? Do you enjoy interacting with people of all ages and backgrounds? If you have the passion and interest in leading volunteers at Sunday Streets, become a Volunter Captain!"
+)
+
+Event.create(
+    name: "Have fun in the sun while volunteering at the Friends Steps Book Sale!",
+    date: "02/03/2015",
+    address: "100 Larkin St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94102",
+    description: "About Friends of the San Francisco Public Library's Steps Book Sales are held on Wednesdays (April - Oct.) on the Larkin Street steps of the San Francisco Public Library (Main). Volunteers work in 2 shifts: 10:15 am - 1 pm and 1 pm - 4 pm. The morning volunteers will set up tables and load books from boxes ont the tables. The afternoon group will pack up any leftover books and fold up the tables. Both shifts will help sell books."
+)
+
+Event.create(
+    name: "Volunteer - Donation Pick-ups",
+    date: "02/08/2015",
+    address: "438 Treat actively",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94110",
+    description: "FRIENDS of the San Francisco Public Library is seeking volunteers to pick up donations of fewer than 8 boxes of books and bring them to the Donation Center. Are you available to help? You will need your own car and should be able to 25 pounds. If you can help, please contact me. I will send you the name and phone number of the book donor. You can arrange a time to pick up the donated books and bring them to the Book Donation Center, 438 Treat Avenue, in the Mission. Please let me know if you are able to lend a hand. Thanks, Donalda Membership/Volunteer Services Manager"
+)
+
+Event.create(
+    name: "Mentor Middle School Students at the Beacon this Spring!",
+    date: "04/02/2015",
+    address: "450 Church St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94114",
+    description: "The Mission Community Beacon at Everett Middle School provides after-school and summer programming, school-day support, and parent engagement through collaboration with program partners. Program Volunteer: Mission Graduates is seeking volunteers to give our students individual attention to help them succeed. Volunteers help students with their class work, participate in academic enrichment activities and support our Classroom Coordinators with daily program plans. Everett Middle School (450 Church Street, SF CA 94114) 3:30-6:30pm Mon-Fri."
+)
+
+Event.create(
+    name: "Volunteer with Mission youth at Marshall Elementary",
+    date: "05/26/2015",
+    address: "1575 15th St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94103",
+    description: "Mission Graduates is a nonprofit organization that increases the number of K-12 students in San Francisco’s Mission District who are prepared for and complete a college education. Extended DayProgram (EDP) is an after-school program that focuses on positive youth development outcomes and helping needy children excel academically. The EDP serves 400 children across three sites, spanning K-5th grades. The EDP at Marshall Elementary School (1575 15th Street, SF CA 94103) provides after-school programs from 2:40-6:00 pm Monday-Friday. Mission Graduates is ..."
+)
+
+Event.create(
+    name: "College Connect 2013-14 Volunteer Internship",
+    date: "03/01/2015",
+    address: "522 Valencia St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94110",
+    description: "Mission Graduates is a community-based non-profit that works to increase the number of K-12 students in San Francisco's Mission District who are prepared for and complete a college education. College Connect is a family-based college-counseling program for low-income youth who will be first-generation to attend college. The program is designed to ensure that deserving students who meet specified criteria receive the guidance they need to choose the right college, gain acceptance, and graduate with a college degree. We are looking for self-motivated individuals who are familiar with the Mission community, are bilingual in Spanish or Cantonese (preferable, but not required) and have ..."
+)
+
+Event.create(
+    name: "Show the Love - photograph mentors at our Appreciation EVENT",
+    date: "02/29/2015",
+    address: "144 Taylor St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94102",
+    description: "Every year hundreds of mentors support vulnerable youth in San Francisco's public schools. Mentoring For Success wants to show the love by photographing these mentoring matches and sharing this movement with the public. We expect to offer the photos to the mentoring matches as a memoir of their time together. We also expect the series of photos to be used in outreach materials, newsletters, on our website, www.healthiersf.org/mentoringforsuccess, and in exhibits throughout the community. WE ARE LOOKING FOR PHOTOGRPAHERS TO PHOTOGRPAH MENOTRING MATCHES ..."
+)
+
+Event.create(
+    name: "Peer Advocate",
+    date: "02/07/2015",
+    address: "1360 Mission St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94103",
+    description: "SDA has a long history of supporting our community members using the peer support, empowerment approach. The meaning of peer can vary - you might be a peer in terms of gender, race, cultural background, age or disability. In this context, Peer Advocates use their own experiences to support other seniors and people with disabilities in navigating the systems of support that we need to live safely and with dignity and be a vital part of our communities. Peer support has long been understood to be a very empowering approach, since people tend to identify with and trust other people who have struggled with similar issues, especially when discrimination ..."
+)
+
+Event.create(
+    name: "Want to volunteer at Sunday Streets? First attend an orientation and see how you can get involved!",
+    date: "02/01/2015",
+    address: "433 Natoma St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94103",
+    description: "Become a volunteer for Sunday Streets by attending a volunteer orientation and learn more about how you can get involved!  Sunday Streets encourages health, recreation, community activities and fun in San Francisco. On once a month Sundays, we transform miles of car-free streets into recreational space for the day in diverse San Francisco neighborhoods. We bring these temporary car-free spaces to neighborhoods that lack recreational parks and outdoor activities. By closing the streets down, we’re opening them for people to bike, walk, run, dance, do yoga, or do ..."
+)
+
+Event.create(
+    name: "Elders Need Helpers in the San Francisco Community: Volunteer to Help Seniors Today!",
+    date: "02/01/2015",
+    address: "155 Market St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94102",
+    description: "Volunteer to help elders for the activities you enjoy, on a schedule that works best for you. Go to ElderHelpers.org to create a volunteer profile and start today!"
+)
+
+Event.create(
+    name: "Openhouse has Data Entry Opportunities galore!",
+    date: "02/14/2015",
+    address: "1800 Market Street",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94102",
+    description: "Openhouse ( www.openhouse-sf.org) has data entry opportunities and the schedule is flexible! Data entry opportunities includes: * entering information from volunteer applications into volunteer software database * Entering housing workshop evaluations into software designated for that purpose *Entering information and referral into specific database software * Entering information into our special software databases. We are looking for volunteers who are tech savvy, comfortable learning new software databases. Interested? Sure, why not? Call or email Openhouse! (415)296-8995 ext 310 or suzi@openhouse-sf.org."
+)
+
+Event.create(
+    name: "Parent Partner Program Assistant Internship",
+    date: "01/31/2015",
+    address: "3040 16th St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94103",
+    description: "The objective of the Parent Partner Program is to provide monolingual Spanish-speaking and/or immigrant parents with the information, training and confidence to be more actively engaged in supporting their children’s academic success. We accomplish this by offering a range of workshops for parents onsite at target Mission District public schools. We also offer professional development and coaching to a team of parent leaders from these schools that provide logistical and outreach support for our school-based workshops."
+)
+
+Event.create(
+    name: "Volunteer Leadership Position: Explore San Francisco and build community",
+    date: "03/04/2015",
+    address: "466 Geary St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94102",
+    description: "Are you a natural leader overflowing with energy? Do you love San Francsico and Sunday Streets (Don't know Sunday Streets? - check it out here: www.sundaystreetssf.com/video-gallery)? Do you enjoy interacting with people of all ages and backgrounds? If you have the passion and interest in leading volunteers at Sunday Streets, become a Volunter Captain!"
+)
+
+Event.create(
+    name: "Have fun in the sun while volunteering at the Friends Steps Book Sale!",
+    date: "02/03/2015",
+    address: "100 Larkin St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94102",
+    description: "About Friends of the San Francisco Public Library's Steps Book Sales are held on Wednesdays (April - Oct.) on the Larkin Street steps of the San Francisco Public Library (Main). Volunteers work in 2 shifts: 10:15 am - 1 pm and 1 pm - 4 pm. The morning volunteers will set up tables and load books from boxes ont the tables. The afternoon group will pack up any leftover books and fold up the tables. Both shifts will help sell books."
+)
+
+Event.create(
+    name: "Volunteer - Donation Pick-ups",
+    date: "02/08/2015",
+    address: "438 Treat actively",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94110",
+    description: "FRIENDS of the San Francisco Public Library is seeking volunteers to pick up donations of fewer than 8 boxes of books and bring them to the Donation Center. Are you available to help? You will need your own car and should be able to 25 pounds. If you can help, please contact me. I will send you the name and phone number of the book donor. You can arrange a time to pick up the donated books and bring them to the Book Donation Center, 438 Treat Avenue, in the Mission. Please let me know if you are able to lend a hand. Thanks, Donalda Membership/Volunteer Services Manager"
+)
+
+Event.create(
+    name: "Mentor Middle School Students at the Beacon this Spring!",
+    date: "04/02/2015",
+    address: "450 Church St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94114",
+    description: "The Mission Community Beacon at Everett Middle School provides after-school and summer programming, school-day support, and parent engagement through collaboration with program partners. Program Volunteer: Mission Graduates is seeking volunteers to give our students individual attention to help them succeed. Volunteers help students with their class work, participate in academic enrichment activities and support our Classroom Coordinators with daily program plans. Everett Middle School (450 Church Street, SF CA 94114) 3:30-6:30pm Mon-Fri."
+)
+
+Event.create(
+    name: "Volunteer with Mission youth at Marshall Elementary",
+    date: "05/26/2015",
+    address: "1575 15th St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94103",
+    description: "Mission Graduates is a nonprofit organization that increases the number of K-12 students in San Francisco’s Mission District who are prepared for and complete a college education. Extended DayProgram (EDP) is an after-school program that focuses on positive youth development outcomes and helping needy children excel academically. The EDP serves 400 children across three sites, spanning K-5th grades. The EDP at Marshall Elementary School (1575 15th Street, SF CA 94103) provides after-school programs from 2:40-6:00 pm Monday-Friday. Mission Graduates is ..."
+)
+
+Event.create(
+    name: "College Connect 2013-14 Volunteer Internship",
+    date: "03/01/2015",
+    address: "522 Valencia St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94110",
+    description: "Mission Graduates is a community-based non-profit that works to increase the number of K-12 students in San Francisco's Mission District who are prepared for and complete a college education. College Connect is a family-based college-counseling program for low-income youth who will be first-generation to attend college. The program is designed to ensure that deserving students who meet specified criteria receive the guidance they need to choose the right college, gain acceptance, and graduate with a college degree. We are looking for self-motivated individuals who are familiar with the Mission community, are bilingual in Spanish or Cantonese (preferable, but not required) and have ..."
+)
+
+Event.create(
+    name: "Show the Love - photograph mentors at our Appreciation EVENT",
+    date: "02/29/2015",
+    address: "144 Taylor St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94102",
+    description: "Every year hundreds of mentors support vulnerable youth in San Francisco's public schools. Mentoring For Success wants to show the love by photographing these mentoring matches and sharing this movement with the public. We expect to offer the photos to the mentoring matches as a memoir of their time together. We also expect the series of photos to be used in outreach materials, newsletters, on our website, www.healthiersf.org/mentoringforsuccess, and in exhibits throughout the community. WE ARE LOOKING FOR PHOTOGRPAHERS TO PHOTOGRPAH MENOTRING MATCHES ..."
+)
+
+Event.create(
+    name: "Peer Advocate",
+    date: "02/07/2015",
+    address: "1360 Mission St, Suite 400",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94103",
+    description: "SDA has a long history of supporting our community members using the peer support, empowerment approach. The meaning of peer can vary - you might be a peer in terms of gender, race, cultural background, age or disability. In this context, Peer Advocates use their own experiences to support other seniors and people with disabilities in navigating the systems of support that we need to live safely and with dignity and be a vital part of our communities. Peer support has long been understood to be a very empowering approach, since people tend to identify with and trust other people who have struggled with similar issues, especially when discrimination ..."
+)
+
+Event.create(
+    name: "Want to volunteer at Sunday Streets? First attend an orientation and see how you can get involved!",
+    date: "02/01/2015",
+    address: "433 Natoma St",
+    city: "San Francisco",
+    state: "CA",
+    zipcode: "94103",
+    description: "Become a volunteer for Sunday Streets by attending a volunteer orientation and learn more about how you can get involved!  Sunday Streets encourages health, recreation, community activities and fun in San Francisco. On once a month Sundays, we transform miles of car-free streets into recreational space for the day in diverse San Francisco neighborhoods. We bring these temporary car-free spaces to neighborhoods that lack recreational parks and outdoor activities. By closing the streets down, we’re opening them for people to bike, walk, run, dance, do yoga, or do ..."
+)
